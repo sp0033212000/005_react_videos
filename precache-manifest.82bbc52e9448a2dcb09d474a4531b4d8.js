449e80bb26550188bab3bc3f6f024fbeb4f22a8a
Liao Ji Wei <liaojiwei@Liaode-MacBook-Pro.local>
2019-12-01 18:02:26 +0800

    Updates

---
+++ precache-manifest.82bbc52e9448a2dcb09d474a4531b4d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464f6d5c6db1850d670670e793b0bdf3",
+    "url": "/005_react_videos/index.html"
+  },
+  {
+    "revision": "2d9042de257a49fc6e81",
+    "url": "/005_react_videos/static/css/main.96c11df6.chunk.css"
+  },
+  {
+    "revision": "4b77446d1d38f77ff332",
+    "url": "/005_react_videos/static/js/2.636f9ff4.chunk.js"
+  },
+  {
+    "revision": "2d9042de257a49fc6e81",
+    "url": "/005_react_videos/static/js/main.c3685ad4.chunk.js"
+  },
+  {
+    "revision": "13ea7cc08d2189a52121",
+    "url": "/005_react_videos/static/js/runtime-main.74190d2c.js"
+  }
+]);
